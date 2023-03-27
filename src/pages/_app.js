2a625a1dc064c4bb480c47082a22ff9975ad3e19
Scott Anderson <mail@scottanderson.com.au>
2023-03-27 12:44:04 +1100

    initial

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import { withFronteggApp } from "@frontegg/nextjs/pages";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function CustomApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
+
+export default withFronteggApp(CustomApp, {
+  hostedLoginBox: true,
+  authOptions: {
+    // keepSessionAlive: true, // Uncomment this in order to maintain the session alive
+  },
+});
